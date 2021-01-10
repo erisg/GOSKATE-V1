c4b66cbf756457989b6fcf2913ae651a3a966a05
erisg <erisbg.513@gmail.com>
2021-01-09 21:35:50 -0500

    *Creacion dimens.xml *Creacion strings.xml para es y en *Todos los xml actuales quedaron con dimenas y strings *Creacion menu

--- app/src/main/java/go/goskate/goskate/ui/MainActivity.kt
+++ app/src/main/java/go/goskate/goskate/ui/MainActivity.kt
@@ -1,12 +1,28 @@
 package go.goskate.goskate.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import go.goskate.goskate.R
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpNavigation()
+
+    }
+
+    fun setUpNavigation() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navigation_goskate) as NavHostFragment?
+        NavigationUI.setupWithNavController(
+            bottomNavigationView,
+            navHostFragment!!.navController
+        )
     }
 }
