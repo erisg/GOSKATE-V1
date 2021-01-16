f1ef258d83b3336abc71fbe1c16b991182c56e5a
erisg <erisbg.513@gmail.com>
2021-01-15 20:37:06 -0500

    *implementacion google maps api

--- app/src/main/java/go/goskate/goskate/ui/Maps.kt
+++ app/src/main/java/go/goskate/goskate/ui/Maps.kt
@@ -1,18 +1,25 @@
 package go.goskate.goskate.ui
 
+import android.content.res.Resources
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import go.goskate.goskate.R
 import go.goskate.goskate.ui.viewmodel.MapsViewModel
 
-class Maps : Fragment(), OnMapReadyCallback {
+class Maps : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+
+    private lateinit var mMap: GoogleMap
+    private var mapView: MapView? = null
 
     companion object {
         fun newInstance() = Maps()
@@ -32,8 +39,11 @@ class Maps : Fragment(), OnMapReadyCallback {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        supportFragmentManager.childFragmentManager.findFragmentById(R.id.mapFrameLayout)
-        supportFragmentManager.getMapAsync(this)
+
+        mapView = view.findViewById(R.id.mapFrameLayout) as MapView
+        mapView!!.onCreate(savedInstanceState)
+        mapView!!.onResume()
+        mapView!!.getMapAsync(this)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -42,7 +52,28 @@ class Maps : Fragment(), OnMapReadyCallback {
         // TODO: Use the ViewModel
     }
 
-    override fun onMapReady(p0: GoogleMap?) {
+    override fun onMapReady(googleMap: GoogleMap?) {
+        mMap = googleMap!!
+        mMap.setOnMarkerClickListener(this)
+
+        try {
+            val success = mMap.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(this.context, R.raw.mapstyle)
+            )
+            if (!success) {
+                Log.e("oo", "Style parsing failed.")
+            }
+        } catch (e: Resources.NotFoundException) {
+            Log.e("oo", "Can't find style. Error: ", e)
+        }
+
+        val place = LatLng(4.648671, -74.120623)
+        val zoomLevel = 11.8f
+        mMap.addMarker(MarkerOptions().position(place).title("loveu"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(place, zoomLevel))
+    }
+
+    override fun onMarkerClick(p0: Marker?): Boolean {
         TODO("Not yet implemented")
     }
 
